55ae3b69d417b398b70b782c6242e58896bd9832
pouster52 <https://github.com/pouster52>
2025-06-30 19:20:01 -0300

    agora temos templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def portao(request):
+    return HttpResponse("voce chegou ao portao da casa")
+
+def sala(request):
+    return HttpResponse("voce chegou na sala,senta no sofa")
+
+def quarto(request):
+    return HttpResponse("agora esta no quarto vai varre")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})

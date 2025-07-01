a56fdbf7711af03d7646caf99023fc01d90c2823
pouster52 <https://github.com/pouster52>
2025-07-01 15:00:27 -0300

    CSS, HTML e tudo

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('portao',views.portao),
-    path('sala',views.sala),
-    path('quarto',views.quarto),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def portao(request):
-    return HttpResponse("voce chegou ao portao da casa")
-
-def sala(request):
-    return HttpResponse("voce chegou na sala,senta no sofa")
-
-def quarto(request):
-    return HttpResponse("agora esta no quarto vai varre")
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {"posts":posts})
